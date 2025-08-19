dd41c6cb7ef752554f2b206106c55d4c65a26510
Justin Faris <jwfaris@asu.edu>
2025-08-19 10:03:58 -0700

    Delete re-intro/level-1/wrapper.c

--- re-intro/level-1/wrapper.c
+++
@@ -1,7 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-  execl("/usr/bin/python3", "python3", "/challenge/.hidden_runtest.py", argv[1], NULL);
-  return 0;
-}
