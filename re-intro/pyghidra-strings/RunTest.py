7ede3f124f3ab6ff74342c9f72a997ec70a72dbb
Justin Faris <jwfaris@asu.edu>
2025-09-21 17:13:04 -0700

    Create RunTest.py

---
+++ re-intro/pyghidra-strings/RunTest.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+import pyghidra as pgh
+
+pgh.start()
+with pgh.open_project(sys.argv[1]) as proj:
+    pdata = proj.getProjectData()
+    root = pdata.getRootFolder()
+    df = [f for f in root.getFiles() if f.getFileType() == "Program"][0]
+    program = proj.openProgram(df)
+    mem = program.getMemory()
+    start = mem.getMinAddress()
+    was = mem.findBytes(start, bytearray(b"absent"), 0, None)
+    now = mem.findBytes(start, bytearray(b"present"), 0, None)
+    if was is None and now is not None:
+        print("flag{present_detected}")
+    proj.close(program)
+pgh.shutdown()
