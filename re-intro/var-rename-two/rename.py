884c23d2c303d67eaf2be6e6025d7026e98abc5c
Justin Faris <jwfaris@asu.edu>
2026-02-09 13:06:02 -0700

    Update rename.py

--- re-intro/var-rename-two/rename.py
+++ re-intro/var-rename-two/rename.py
@@ -3,6 +3,7 @@ from ghidra.framework.model import DomainObjectListener
 from ghidra.app.util.exporter import XmlExporter
 from java.lang import Thread
 from java.io import File
+import sys
 import subprocess
 
 # Globals
@@ -18,8 +19,7 @@ done_parameter = False
 done_function = False
 
 print("In this challenge, you will have to rename several parts of the binary in order to get the flag.")
-print("The first portion of the executable you should rename is the variable 'target'. Locate this variable inside of main and rename it to 'pwned_var'.")
-
+print("The first portion of the executable you should rename is the variable 'global_target'. Locate this variable inside of main and rename it to 'pwned_var'.")
     
 class ProgListener(DomainObjectListener):
     def domainObjectChanged(self, ev):
@@ -27,12 +27,9 @@ class ProgListener(DomainObjectListener):
         global done_parameter
         global done_function
 
-        #print("change detected")
-
         if not done_var:
             sym = currentProgram.getSymbolTable().getPrimarySymbol(VAR_TARGET_ADDR)
             if sym and sym.getName() == VAR_EXPECTED_NAME:
-                #print("Correct change: %s -> %s" % (VAR_TARGET_ADDR, VAR_EXPECTED_NAME))
                 print("Correct! You can also rename functions just like with variables. Locate the function named 'functiontarget' and rename it to 'pwnedfunction'.")
                 done_var = True
             return
@@ -40,7 +37,6 @@ class ProgListener(DomainObjectListener):
         if done_var and not done_function:
             sym = currentProgram.getSymbolTable().getPrimarySymbol(FUNC_TARGET_ADDR)
             if sym and sym.getName() == FUNC_EXPECTED_NAME:
-                #print("Correct change: %s -> %s" % (FUNC_TARGET_ADDR, FUNC_EXPECTED_NAME))
                 print("Correct! Finally, just like with variables declared outside of functions, you can also rename function parameters. Locate the parameter of 'pwnedfunction' and rename it to 'pwned_param'.")
                 done_function = True
             return
@@ -50,14 +46,12 @@ class ProgListener(DomainObjectListener):
             if func:
                 params = func.getParameters()
                 if params and params[0].getName() == PARAM_EXPECTED_NAME:
-                    #print("Correct change: parameter -> %s" % (PARAM_EXPECTED_NAME))
                     print("Correct! You have successfully completed the rename challenge")
                     done_parameter = True
             return
     
 listener = ProgListener()
 currentProgram.addListener(listener)
-#print("Monitoring: " + currentProgram.getName())
     
 while not monitor.isCancelled() and not (done_var and done_parameter and done_function):
     Thread.sleep(1000)
@@ -70,9 +64,10 @@ output_file = File("/tmp/rename_challenge_completed.xml")
 exporter.export(output_file, program, None, getMonitor())
 
 # Call python file with elevated privileges to read /tmp/rename_challenge_completed.xml and get the flag if correct
-result = subprocess.run(['/challenge/CheckXML.py'], capture_output=True, text=True)
+result = subprocess.Popen(['/challenge/CheckXML.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = result.communicate()
 
 # Grab the flag from the python file
 # Print the flag to the Ghidra console standard output
-if result.returncode == 0 and result.stdout:
-    print(result.stdout)
+if result.returncode == 0 and out:
+    print(out)
