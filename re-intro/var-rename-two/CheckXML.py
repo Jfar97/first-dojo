7591b3e898aab835706301fe022d08bd775e4e02
Justin Faris <jwfaris@asu.edu>
2026-02-09 12:50:23 -0700

    Create CheckXML.py

---
+++ re-intro/var-rename-two/CheckXML.py
@@ -0,0 +1,27 @@
+#!/usr/bin/exec-suid -- /usr/local/bin/python -I
+
+edited_global = "SYMBOL ADDRESS=\"00104018\" NAME=\"pwned_var\" NAMESPACE=\"\" TYPE=\"global\" SOURCE_TYPE=\"USER_DEFINED\" PRIMARY=\"y\""        
+edited_function = "SYMBOL ADDRESS=\"00101139\" NAME=\"pwnedfunction\" NAMESPACE=\"\" TYPE=\"global\" SOURCE_TYPE=\"USER_DEFINED\" PRIMARY=\"y\""      
+edited_param = "REGISTER_VAR NAME=\"pwned_param\" REGISTER=\"EDI\" DATATYPE=\"undefined4\" DATATYPE_NAMESPACE=\"/\""     
+
+global_edited = False
+function_edited = False
+param_edited = False
+
+correct_edits = False
+
+with open("/tmp/rename_challenge_completed.xml", "r", encoding="UTF-8") as file:
+    for line in file:
+        if edited_global in line:
+            global_edited = True
+        if edited_function in line:
+            function_edited = True
+        if edited_param in line:
+            param_edited = True
+        if global_edited and function_edited and param_edited:
+            correct_edits = True
+            break
+
+if correct_edits:
+    with open("/flag") as flag:
+        print(flag.read())
