450fcbcc166732655d454680b9ab5147bd66aaa9
Justin Faris <jwfaris@asu.edu>
2026-03-02 16:11:56 -0700

    Update CheckXML.py

--- re-intro/var-rename-two/CheckXML.py
+++ re-intro/var-rename-two/CheckXML.py
@@ -1,8 +1,18 @@
 #!/usr/bin/exec-suid -- /usr/local/bin/python -I
 import os
 
-edited_global = "SYMBOL ADDRESS=\"00104018\" NAME=\"pwned_var\" NAMESPACE=\"\" TYPE=\"global\" SOURCE_TYPE=\"USER_DEFINED\" PRIMARY=\"y\""        
-edited_function = "SYMBOL ADDRESS=\"00101139\" NAME=\"pwnedfunction\" NAMESPACE=\"\" TYPE=\"global\" SOURCE_TYPE=\"USER_DEFINED\" PRIMARY=\"y\""      
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+addresses_file_path = os.path.join(curr_dir, "addresses.txt")
+
+global_target_addr = ""
+function_target_addr = ""
+
+with open(addresses_file_path, "r") as f:
+    global_target_addr = f.readline().strip()
+    function_target_addr = f.readline().strip()
+
+edited_global = f"SYMBOL ADDRESS=\"{global_target_addr}\" NAME=\"pwned_var\" NAMESPACE=\"\" TYPE=\"global\" SOURCE_TYPE=\"USER_DEFINED\" PRIMARY=\"y\""        
+edited_function = f"SYMBOL ADDRESS=\"{function_target_addr}\" NAME=\"pwnedfunction\" NAMESPACE=\"\" TYPE=\"global\" SOURCE_TYPE=\"USER_DEFINED\" PRIMARY=\"y\""      
 edited_param = "REGISTER_VAR NAME=\"pwned_param\" REGISTER=\"EDI\" DATATYPE=\"undefined4\" DATATYPE_NAMESPACE=\"/\""     
 
 global_edited = False
@@ -23,4 +33,3 @@ with open("/tmp/rename_challenge_completed.xml", "r", encoding="UTF-8") as file:
 print("euid:", os.geteuid())
 with open("/flag", "r") as flag:
     print(flag.read())
-#print("flag achieved")
