04f875e0fa39bc298300891902eea0649bd45119
Justin Faris <jwfaris@asu.edu>
2026-03-12 16:29:04 -0700

    Update Autoscript.py

--- re-intro/auto-script-test/Autoscript.py
+++ re-intro/auto-script-test/Autoscript.py
@@ -1,3 +1,10 @@
 #@runtime Jython
+from ghidra.framework.model import DomainObjectListener
 
-print("Hello World")
+class MyListener(DomainObjectListener):
+    def domainObjectChanged(self, ev):
+        print("change detected")
+
+listener = MyListener()
+currentProgram.addListener(listener)
+print("Monitoring: " + currentProgram.getName())
